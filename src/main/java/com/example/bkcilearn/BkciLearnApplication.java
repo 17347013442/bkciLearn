63cb993270c6a5fe2b10383a1a80618b0ed19a88
1214000999@qq.com <sdsgsdg>
2021-12-27 17:46:16 +0800

    Initial commit

---
+++ src/main/java/com/example/bkcilearn/BkciLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.bkcilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkciLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BkciLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bkcilearn/BkciLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bkcilearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkciLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
